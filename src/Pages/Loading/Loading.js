f05d0d27c74e88444754558c383c1597ecefeca5
sabbirpboss <sabbir.ah799@gmail.com>
2022-05-10 20:02:35 +0600

    spinner style added

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -3,7 +3,7 @@ import { Spinner } from "react-bootstrap";
 
 const Loading = () => {
   return (
-    <div>
+    <div className="my-5">
       <Spinner animation="border" role="status" variant="info">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
